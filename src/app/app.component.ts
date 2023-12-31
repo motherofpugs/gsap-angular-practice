c0218f2a552202bfe3dec7a1ebc6d299befe7d0e
motherofpugs <csampaidominika@gmail.com>
2024-01-01 00:43:27 +0100

    added gsap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,7 +33,135 @@ export class AppComponent implements OnInit {
     this.inititalAnimations(), this.initScrollAnimations();
   }
 
-  initScrollAnimations(): void {}
+  initScrollAnimations(): void {
+    gsap.to(this.imageFirst.nativeElement, {
+      scrollTrigger: {
+        trigger: this.imageFirst.nativeElement,
+        scrub: true,
+
+        start: '110% center',
+      } as gsap.plugins.ScrollTriggerInstanceVars,
+      duration: 1.1,
+      scale: 1.2,
+      height: 250,
+    });
+    gsap.to(this.imageSecond.nativeElement, {
+      scrollTrigger: {
+        trigger: this.imageSecond.nativeElement,
+        scrub: true,
+        start: '80% center',
+      } as gsap.plugins.ScrollTriggerInstanceVars,
+      duration: 1.1,
+      scale: 1.2,
+      height: 380,
+    });
+    gsap.to(this.document.querySelector('heading-1'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('heading-1'),
+        scrub: true,
+        start: '150% center',
+      },
+      color: '#fff',
+      duration: 1.5,
+    });
+    gsap.to(this.document.querySelector('.paragraph'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('.paragraph'),
+        scrub: true,
+        start: '150% center',
+      },
+      color: '#fff',
+      duration: 1.5,
+    });
+    gsap.to(this.document.querySelector('.btn'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('.btn'),
+        scrub: true,
+        start: '150% center',
+      },
+      color: '#fff',
+      duration: 1.5,
+    });
+    gsap.from(this.document.querySelector('#buy'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('#buy'),
+        scrub: true,
+        toggleClass: 'active',
+        start: 'top center',
+      },
+      duration: 1.5,
+      y: 40,
+      opacity: 0,
+    });
+    gsap.from(this.document.querySelector('#about'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('#about'),
+        scrub: true,
+        toggleClass: 'active',
+        start: 'top center',
+      },
+      duration: 1.5,
+      y: 40,
+      opacity: 0,
+    });
+    gsap.from(this.document.querySelector('.box'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('.box'),
+        scrub: true,
+        toggleClass: 'active',
+        start: '-10% center',
+      },
+      duration: 1.5,
+      width: 0,
+      opacity: 0,
+    });
+    gsap.from(this.document.querySelector('.info-1__visual img'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('.info-1__visual img'),
+        scrub: true,
+        toggleClass: 'active',
+        start: '-60% bottom',
+      },
+      duration: 1.5,
+      height: 0,
+      scale: 1.3,
+      opacity: 0,
+    });
+    gsap.from(this.document.querySelector('.quote'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('.quote'),
+        scrub: true,
+        toggleClass: 'active',
+        start: '-60% bottom',
+      },
+      duration: 1.5,
+
+      opacity: 0,
+    });
+    gsap.from(this.document.querySelector('.info-1__visual .heading-3'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('.info-1__visual .heading-3'),
+        scrub: true,
+        toggleClass: 'active',
+        start: '-60% bottom',
+      },
+      duration: 1.5,
+      y: 40,
+      opacity: 0,
+    });
+    gsap.from(this.document.querySelector('.info-1__visual .btn--learn'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('.info-1__visual .btn--learn'),
+        scrub: true,
+        toggleClass: 'active',
+        start: '-60% bottom',
+      },
+      duration: 1.5,
+      y: 40,
+      opacity: 0,
+    });
+  }
+
   inititalAnimations(): void {
     gsap.from(this.menu.nativeElement.childNodes, {
       duration: 0.5,
@@ -41,6 +169,43 @@ export class AppComponent implements OnInit {
       y: -20,
       stagger: 0.2,
       delay: 0.5,
+    });
+    gsap.from(this.menuSecond.nativeElement.childNodes, {
+      duration: 0.5,
+      opacity: 0,
+      y: -20,
+      stagger: 0.2,
+      delay: 0.8,
+    });
+    gsap.from(this.imageFirst.nativeElement, {
+      duration: 0.7,
+      opacity: 0,
+      y: -30,
+      delay: 0.5,
+    });
+    gsap.from(this.imageSecond.nativeElement, {
+      duration: 0.7,
+      opacity: 0,
+      y: -30,
+      delay: 0.6,
+    });
+    gsap.from(this.document.querySelector('.heading-1'), {
+      duration: 0.7,
+      opacity: 0,
+      y: -30,
+      delay: 0.6,
+    });
+    gsap.from(this.document.querySelector('.paragraph'), {
+      duration: 0.7,
+      opacity: 0,
+      y: -30,
+      delay: 0.7,
+    });
+    gsap.from(this.document.querySelector('.btn'), {
+      duration: 0.7,
+      opacity: 0,
+      y: -30,
+      delay: 0.8,
     });
   }
 }
