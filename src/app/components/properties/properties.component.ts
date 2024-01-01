967ff8d527034936077142647e40fa522b6b03ac
motherofpugs <csampaidominika@gmail.com>
2024-01-02 00:00:21 +0100

    added properties

--- src/app/components/properties/properties.component.ts
+++ src/app/components/properties/properties.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { Observable, combineLatest, map } from 'rxjs';
 import { PropertyModel } from 'src/app/models/property.model';
 import { PropertyService } from 'src/app/services/property.service';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-properties',
   templateUrl: './properties.component.html',
   styleUrls: ['./properties.component.scss'],
 })
-export class PropertiesComponent implements OnInit {
+export class PropertiesComponent implements OnInit, AfterViewInit {
+  @ViewChild('row') row!: ElementRef<HTMLDivElement>;
+
   constructor(private propertyService: PropertyService) {}
   allProperties$!: Observable<PropertyModel[]>;
   rentedProperties$!: Observable<PropertyModel[]>;
@@ -41,5 +54,22 @@ export class PropertiesComponent implements OnInit {
         ...notRentedProperties,
       ])
     );
+  }
+
+  ngAfterViewInit(): void {
+    this.inititalAnimations();
+  }
+
+  inititalAnimations(): void {
+    console.log('Row Element:', this.row);
+    console.log('Child Nodes:', this.row.nativeElement.childNodes);
+    gsap.from(this.row.nativeElement, {
+      opacity: 0,
+      scale: 0.5,
+      y: 30,
+      duration: 1.5,
+      stagger: 0.2,
+      delay: 0.5,
+    });
   }
 }
